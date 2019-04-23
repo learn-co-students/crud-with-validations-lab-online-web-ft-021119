ba03416684c37ccebae665226322d19e12a14884
Lizzie <lchan217@gmail.com>
2019-04-23 08:07:40 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,6 +5,6 @@ class Song < ApplicationRecord
   validates :release_year, presence: true, numericality: { less_than_or_equal_to: Date.today.year }, if: :released_is_true?
 
   def released_is_true?
-    released == true
+    released
   end
 end
