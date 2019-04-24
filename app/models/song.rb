50ccaee89985b1c62db35191b34dcc23b04a3739
ChrstphGrnr <ChristophGruener@me.com>
2019-04-24 12:47:13 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,25 @@
 class Song < ApplicationRecord
-    validates :title, presence: :true
-    validates_inclusion_of :released, in: [true, false]
-    validates :artist_name, presence: :true
-    validates_with ReleaseYearValidator
-    validates_with SameYearValidator
-    validates_with FutureReleaseValidator    
+    validates_presence_of :title, :artist_name
+    validates :release_year, presence: true, inclusion: {in: 1..Time.now.year}, if: -> {has_released?}
+    validates_uniqueness_of :title, if: -> {existing_artist?}
+ 
+    private
+    def has_released?
+       !!self.released
+    end
+ 
+    def existing_artist?
+       artist = Song.find_by_artist_name(self.artist_name)
+    end
+ end
 
-end
+
+
+# class Song < ApplicationRecord
+#     validates :title, presence: :true
+#     validates :artist_name, presence: :true
+#     validates_with ReleaseYearValidator
+#     validates_with SameYearValidator
+#     validates_with FutureReleaseValidator    
+
+# end
